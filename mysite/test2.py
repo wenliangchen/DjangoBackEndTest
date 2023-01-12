1857b6944f6dd0371fd7ced66934fe57ef180c18
Wiliam Chen <wiliamchensde@outlook.com>
2023-01-12 12:59:19 -0600

    Create test2.py

---
+++ mysite/test2.py
@@ -0,0 +1,1 @@
+# https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword#linking-a-pull-request-to-an-issue-using-a-keyword
